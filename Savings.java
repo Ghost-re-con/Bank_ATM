eb8ac1803425c3c449fd9c0d671d9e6869d25297
Dante2346 <132615431+Dante2346@users.noreply.github.com>
2023-05-26 13:40:33 +0100

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,1 +1,17 @@
+package Bank;
 
+public class Savings extends Current{
+
+    public float WithdrawlS(float amount){
+       if(amount > 20000){
+           return 0;
+       }else{
+           return WithdrawlC(amount);
+       }
+    }
+
+    public float DepositS(float amount){
+        return DepositC(amount);
+    }
+
+}
