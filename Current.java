6c4be7a518af8c707319d8728801fee4d2baf430
Esse Kingston Prosperity Esse <132103317+TheAllFather@users.noreply.github.com>
2023-05-26 13:02:00 +0100

    Update Current.java

--- Current.java
+++ Current.java
@@ -1,17 +1,1 @@
-package Bank;
 
-
-public class Current{
-    public float Balance = 1000000;
-
-    public float WithdrawlC(float amount){
-        Balance -= amount;
-        return Balance;
-    }
-
-    public float DepositC(float amount){
-        Balance += amount;
-        return Balance;
-    }
-
-}
